5046b1de878a52e6052ee0e49753c7c992659d29
Sasha Averin <avrn.sasha@gmail.com>
2025-05-06 12:38:52 +0700

    Добавлен headless mode

--- conftest.py
+++ conftest.py
@@ -5,6 +5,7 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture(autouse=True)
 def driver(request):
     options = Options()
+    options.add_argument("--headless")
     options.add_argument("--window-size=1920,1080")
     driver = webdriver.Chrome(options=options)
     request.cls.driver = driver
