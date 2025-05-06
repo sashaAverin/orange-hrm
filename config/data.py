d046f4805861a51cc037107c0051633af378b3e4
Sasha Averin <avrn.sasha@gmail.com>
2025-05-06 12:16:23 +0700

    Добавлены данные для входа нового пользователя

--- config/data.py
+++ config/data.py
@@ -6,3 +6,6 @@ load_dotenv()
 class Data:
     LOGIN = os.getenv("LOGIN")
     PASSWORD = os.getenv("PASSWORD")
+
+    EMPLOYEE_LOGIN = os.getenv("EMPLOYEE_LOGIN")
+    EMPLOYEE_PASSWORD = os.getenv("EMPLOYEE_PASSWORD")
