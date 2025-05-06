6650e9a3ce6b7eed475bf1b01b6e834324794093
Sasha Averin <avrn.sasha@gmail.com>
2025-05-06 11:57:48 +0700

    Инициализирован драйвер

---
+++ conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+@pytest.fixture(autouse=True)
+def driver(request):
+    options = Options()
+    options.add_argument("--window-size=1920,1080")
+    driver = webdriver.Chrome(options=options)
+    request.cls.driver = driver
+    yield driver
+    driver.quit()
