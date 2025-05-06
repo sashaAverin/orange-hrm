d3850e92ee8742baf6066fd04a528cc1df53da7a
Sasha Averin <avrn.sasha@gmail.com>
2025-05-06 11:59:37 +0700

    Создан класс Links. Прописаны url страниц

---
+++ config/links.py
@@ -0,0 +1,6 @@
+class Links:
+    HOST = "https://opensource-demo.orangehrmlive.com/web/index.php/"
+    LOGIN_PAGE = f"{HOST}auth/login"
+    DASHBOARD_PAGE = f"{HOST}dashboard/index"
+    PIM_PAGE = f"{HOST}pim/viewEmployeeList"
+    ADD_EMPLOYEE_PAGE = f"{HOST}pim/addEmployee"
